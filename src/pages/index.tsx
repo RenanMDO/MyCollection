a81183f51738a1b71aa4c5c2a59ab2e8f21d7b4e
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-07-13 12:36:04 -0300

    Criação pagina login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 
 import Head from 'next/head'
-import { useRouter } from "next/router";
+import router from 'next/router';
+import { SubmitHandler, useForm } from 'react-hook-form'
 import styles from '../styles/home.module.scss'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
+type SigninFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object({
+  email: yup.string().required().email(),
+  password: yup.string().required()
+})
 
 export default function Home() {
-  const router = useRouter();
-  const handleSignin = async () => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignin: SubmitHandler<SigninFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
     router.push('/store');
   }
   return (
@@ -18,11 +33,14 @@ export default function Home() {
       <div className={styles.contentContainer}>
         <div className={styles.wrapper}>
           <text>My<span>Collection</span></text>
-          <form action="#">
-            <input className={styles.input} type='email' placeholder='E-MAIL' required />
-            <input className={styles.input} type='password' placeholder='SENHA' required />
+          <form onSubmit={handleSubmit(handleSignin)}>
+            <input className={styles.input} type='email' placeholder='E-MAIL'  {...register('email', { required: true })} />
+            {errors.email && <span>E-mail obrigatório</span>}
+            <input className={styles.input} type='password' placeholder='SENHA'  {...register('password', { required: true })} />
+            {errors.password && <span>Senha obrigatória</span>}
+            <button type='submit'>ENTRAR</button>
           </form>
-          <button type='submit'>ENTRAR</button>
+
           <div className={styles.toggle}>
             <input type="checkbox" id="bar" />
             <label htmlFor="bar"></label>
