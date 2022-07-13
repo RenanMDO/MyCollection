4b327fefc0d0b9bb85f2b39748a1d52ad67b0f76
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-07-13 12:38:14 -0300

    Finalização layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,32 @@
 
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
+
 import '/src/styles/App.scss'
+import styles from './store/store.module.scss'
+
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [active, setActive] = useState(false)
+
+  const handleSlide = () => {
+    setActive(!active)
+  }
+  return (
+    <>
+      <div className={styles.header}>
+        <Sidebar active={active} handleSlide={handleSlide} />
+        <div className={styles.main}>
+          <Header handleSlide={handleSlide} />
+        </div>
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
